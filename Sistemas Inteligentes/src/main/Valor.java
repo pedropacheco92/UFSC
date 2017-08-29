5a17b2357c146202918cb3e4f3408251d8567b16
Pedro Henrique Pacheco <pedropacheco92@outlook.com>
2017-08-29 11:29:24 -0300

    MiniMax

---
+++ Sistemas Inteligentes/src/main/Valor.java
@@ -0,0 +1,16 @@
+package main;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Valor {
+
+	VAZIO("-"),
+	X("X"),
+	O("O");
+	
+	private String value;
+	
+}
