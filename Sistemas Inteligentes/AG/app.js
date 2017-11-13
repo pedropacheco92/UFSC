8ccec1aa4ab80b1b1dd3e4e37f185cea53228cdb
Pedro Henrique Pacheco <pedropacheco92@outlook.com>
2017-11-13 12:02:04 -0200

    wip

--- Sistemas Inteligentes/AG/app.js
+++ Sistemas Inteligentes/AG/app.js
@@ -3,11 +3,16 @@ let target;
 let popmax;
 let mutationRate;
 let population;
+let items;
 
-  function setup() {  
-    target = "To be or not to be."
-    let items = [4, 12, 2, 1, 10, 4, 1, 1, 2, 2]; // pair value-weight
+  function setup() {
+    // valor total da mochila
+    target = 15;
+    // possíveis items, pareados valor-peso
+    items = [4, 12, 2, 1, 10, 4, 1, 1, 2, 2];
+    // população máxima
     popmax = 200;
+    // taxa de mutação
     mutationRate = 0.01;
   
     $(".best").text('-');
