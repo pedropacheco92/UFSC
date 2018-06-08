5dc1d1ae6e63c262b78b44a02ba5898bb63e42cd
Pedro Henrique Pacheco <pedropacheco92@outlook.com>
2018-06-08 12:41:30 -0300

    app

--- BuApp/src/pages/home/home.ts
+++ BuApp/src/pages/home/home.ts
@@ -1,34 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public base64Image: string;
 
-  constructor(public navCtrl: NavController, private camera: Camera) {
-
-    
+  constructor(public navCtrl: NavController) {
   }
 
-  takePicture() {
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    } 
-
-    this.camera.getPicture(options).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI
-      // If it's base64:
-      this.base64Image = 'data:image/jpeg;base64,' + imageData;
-     }, (err) => {
-      // Handle error
-     });
+  emprestimo() {
+    this.navCtrl.push('CameraPage');
   }
 
 }
