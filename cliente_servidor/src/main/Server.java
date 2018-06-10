9885b8adfdb91be39ffb34a2e0b25954a75c45cf
Pedro Henrique Pacheco <pedropacheco92@outlook.com>
2018-06-10 12:11:23 -0300

    wip

--- cliente_servidor/src/main/Server.java
+++ cliente_servidor/src/main/Server.java
@@ -1,6 +1,7 @@
 package main;
 
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -8,23 +9,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Server extends Thread {
-	
+
 	private ServerSocket welcomeSocket;
-	
-	private List<Integer> clients = new ArrayList<>();
-	
+
 	private Integer currentClient;
-	
+
+	private Map<Integer, DataOutputStream> clients = new LinkedHashMap<>();
+
 	@Override
 	public synchronized void start() {
 		super.start();
 		System.out.println("Servidor iniciou");
 	}
-	
+
 	@Override
 	public void run() {
 		try {
@@ -33,43 +35,49 @@ public class Server extends Thread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void createServer() throws Exception {
-		welcomeSocket = new ServerSocket(1);
+		welcomeSocket = new ServerSocket(99);
 
 		while (true) {
-			this.checkClients();			
-			
+			this.checkClients();
 			Socket connectionSocket = welcomeSocket.accept();
 			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-			
-			String clientMsg = inFromClient.readLine();
-			
-			if (clientMsg.equals("token")) {
-				System.out.println("Servidor: " + this.currentClient + " saiu da região crítica!");				
-				this.currentClient = null;
+
+			String clientSentence = inFromClient.readLine();
+			if (clientSentence.equals("token") && nonNull(currentClient)) {
+				System.out.println("Servidor: " + currentClient + " saiu da região crítica!");
+				currentClient = null;
 			} else {
-				Integer client = Integer.parseInt(clientMsg);
-				this.clients.add(client);				
-				System.out.println("Servidor: " + client + " deseja acessar região crítica!");				
+				try {
+					Integer client = Integer.parseInt(clientSentence);
+					currentClient = client;
+					System.out.println("Servidor: " + client + " deseja acessar região crítica!");
+					this.sendToken(client);
+				} catch (NumberFormatException e) {
+					System.out.println("Mensagem desconhecida: " + clientSentence);
+				}
 			}
-			
 		}
+
+	}
+
+	private void sendToken(int port) throws Exception {
+		Socket clientSocket = new Socket("localhost", port);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		outToServer.writeBytes("token");
+		clientSocket.close();
 	}
 
 	private void checkClients() throws IOException {
-		if (!this.clients.isEmpty() && isNull(currentClient)) {
-			Integer client = this.clients.remove(0);
-			this.currentClient = client;
-			this.allowClient(client);			
+		if (isNull(currentClient)) {
+			if (clients.entrySet().iterator().hasNext()) {
+				Entry<Integer, DataOutputStream> entry = clients.entrySet().iterator().next();
+				currentClient = entry.getKey();
+				entry.getValue().writeBytes("token");
+				System.out.println("Servidor: enviado token para " + currentClient);
+			}
 		}
 	}
-	
-	private void allowClient(Integer client) throws IOException {
-		Socket clientSocket = new Socket("localhost", client);
-		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		outToServer.writeBytes("token");
-		System.out.println("Servidor: enviado token para " + client);
-		clientSocket.close();
-	}
+
 }
