f560aecf9dd889636715d6a4d7a8fbccc4a0117f
Pedro <pedropacheco92@outlook.com>
2018-09-14 10:54:18 -0300

    trabalho pronto

--- trabalho1/tarefa11/index.js
+++ trabalho1/tarefa11/index.js
@@ -27,12 +27,20 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
+const hashUser = (user) => {
+    const masterKey = CryptoJS.SHA256(`${user}.${user}`).toString(CryptoJS.enc.Hex);
+
+    return CryptoJS.PBKDF2(user, masterKey, {
+        keySize: 32,
+        interactions: 1000
+    }).toString(CryptoJS.enc.Hex);
+};
+
 app.post('/login', (req, res) => {
     const users = jsonfile.readFileSync(file);
+    const userHashed = hashUser(req.body.user);
 
     for (const user of users) {
-        const key = `${req.body.user}${user.salt}`
-        const userHashed = Base64.encode(CryptoJS.HmacSHA256(req.body.user, key));
         if (userHashed === user.user) {
             const passOk = bcrypt.compareSync(req.body.password, user.password);
             if (passOk === true) {
@@ -41,7 +49,7 @@ app.post('/login', (req, res) => {
                 });
                 return;
             } else {
-                res.render('error.ejs', {
+                res.render('message.ejs', {
                     text: 'Senha incorreta!'
                 });
                 return;
@@ -49,36 +57,30 @@ app.post('/login', (req, res) => {
         }
     }
 
-    res.render('error.ejs', {
+    res.render('message.ejs', {
         text: 'Usuário incorreto!'
     });
 });
 
 app.post('/user', (req, res) => {
     const users = jsonfile.readFileSync(file);
+    const userHashed = hashUser(req.body.user);
 
     for (const user of users) {
-        const key = `${req.body.user}${user.salt}`
-        const userHashed = Base64.encode(CryptoJS.HmacSHA256(req.body.user, key));
         if (userHashed === user.user) {
-            res.render('error.ejs', {
+            res.render('message.ejs', {
                 text: 'Usuário já existe!'
             });
             return;
         }
     }
 
-    const random = Base64.encode(secureRandom(6));
-    const key = `${req.body.user}${random}`;
-    const userHashed = Base64.encode(CryptoJS.HmacSHA256(req.body.user, key));
-
     bcrypt.genSalt(Random.range(8, 13), function (err, salt) {
         bcrypt.hash(req.body.password, salt, function (err, hash) {
 
             const user = {
                 user: userHashed,
-                password: hash,
-                salt: random
+                password: hash
             }
 
             users.push(user);
@@ -87,7 +89,7 @@ app.post('/user', (req, res) => {
                 console.error(err)
             });
 
-            res.render('error.ejs', {
+            res.render('message.ejs', {
                 text: 'Usuário criado!'
             });
         });
@@ -98,10 +100,9 @@ app.post('/user', (req, res) => {
 app.post('/delete-user', (req, res) => {
     const users = jsonfile.readFileSync(file);
     const newUsers = [];
+    const userHashed = hashUser(req.body.user);
 
     for (const user of users) {
-        const key = `${req.body.user}${user.salt}`
-        const userHashed = Base64.encode(CryptoJS.HmacSHA256(req.body.user, key));
         if (userHashed !== user.user) {
             newUsers.push(user);
         }
@@ -111,28 +112,27 @@ app.post('/delete-user', (req, res) => {
         console.error(err);
     });
 
-    res.render('error.ejs', {
+    res.render('message.ejs', {
         text: 'Usuário deletado!'
     });
 });
 
 app.post('/update-user', (req, res) => {
     const users = jsonfile.readFileSync(file);
+    const userHashed = hashUser(req.body.user);
 
     for (const user of users) {
-        const key = `${req.body.user}${user.salt}`
-        const userHashed = Base64.encode(CryptoJS.HmacSHA256(req.body.user, key));
         if (userHashed === user.user) {
             bcrypt.genSalt(Random.range(8, 13), function (err, salt) {
                 bcrypt.hash(req.body.password, salt, function (err, hash) {
-        
+
                     user.password = hash;
-        
+
                     jsonfile.writeFile(file, users, function (err) {
                         console.error(err)
                     });
-        
-                    res.render('error.ejs', {
+
+                    res.render('message.ejs', {
                         text: 'Senha alterada!'
                     });
                 });
